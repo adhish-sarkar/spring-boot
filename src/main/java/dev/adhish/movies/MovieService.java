a4629b8c96f32af0aeecad0f6fb73292668c7695
Adhish Sarkar <sarkaradhish131@gmail.com>
2024-09-09 23:37:02 +0530

    first commit

---
+++ src/main/java/dev/adhish/movies/MovieService.java
@@ -0,0 +1,24 @@
+package dev.adhish.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MovieService {
+
+    @Autowired
+    private MovieRepository movieRepository;
+
+     public List<Movie> allMovies() {
+         System.out.println(movieRepository.findAll().toString());
+        return movieRepository.findAll();
+     }
+
+     public Optional<Movie> findMovieById(String id) {
+         return movieRepository.findByImdbId(id);
+     }
+}
